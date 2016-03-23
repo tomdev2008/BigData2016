bc2f9b3a6ab24807f37faaa92711e6a00c733513
vitaliy-zinchenko <Zinjvi@gmail.com>
2016-03-23 10:24:23 +0000

    c

---
+++ hadoop_hw2/src/main/java/com/epam/App.java
@@ -0,0 +1,41 @@
+package com.epam;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) throws TikaException, SAXException, IOException {
+        System.out.println( "Hello World!" );
+
+
+        InputStream html = IOUtils.toInputStream(
+                        "<html>" +
+                        "<body>" +
+                                "qwe ggg" +
+                                "<script>(function() {console.log('sdf')})()</script>" +
+                                "<br/>" +
+                                "<span>sss</span>" +
+                        "</body>" +
+                        "</html>");
+
+        BodyContentHandler bodyContentHandler = new BodyContentHandler();
+        Metadata metadata = new Metadata();
+        AutoDetectParser parser = new AutoDetectParser();
+        parser.parse(html, bodyContentHandler, metadata, new ParseContext());
+        System.out.println(bodyContentHandler.toString());
+
+
+    }
+}
